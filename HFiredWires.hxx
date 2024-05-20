20ce2df1e037eb429c5f1155fb8a47d48e168f58
Kolas Jedrzej <kjedrzej@lxi085.gsi.de>
2024-05-20 12:05:44 +0200

    Modified program to make it work in analysis

--- HFiredWires.hxx
+++ HFiredWires.hxx
@@ -33,150 +33,53 @@
             {
                 constexpr std::size_t numberOfInnerLayers = 12UL;
                 constexpr std::size_t numberOfOuterLayers = 12UL;
+                constexpr std::size_t numberOfLayersInPlane = 6UL;
+                constexpr std::size_t numberOfPlanes = 4UL;
                 constexpr std::size_t numberOfAllLayers = numberOfInnerLayers + numberOfOuterLayers;
                 constexpr std::array<std::size_t,numberOfInnerLayers> halfLayerIndexing{0,1,2,3,4,5,6,7,8,9,10,11};
                 constexpr std::array<std::size_t,numberOfAllLayers> allLayerIndexing{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
+                constexpr std::array<std::array<std::size_t,numberOfLayersInPlane>,numberOfPlanes> allLayerPerPlaneIndexing
+                    {
+                        std::array<std::size_t,numberOfLayersInPlane>{0,1,2,3,4,5},
+                        std::array<std::size_t,numberOfLayersInPlane>{6,7,8,9,10,11},
+                        std::array<std::size_t,numberOfLayersInPlane>{12,13,14,15,16,17},
+                        std::array<std::size_t,numberOfLayersInPlane>{18,19,20,21,22,23}
+                    };
             }
 
             class HFiredWires
             {
-                private:
-                    std::array<std::vector<int>,WireInfo::numberOfAllLayers> FiredWireCollection;
-                    HCategory *mdc_seg_cat;
-                    HMdcSeg *innerSeg,*outerSeg;
-                    bool MdcCatIsNotSet;
-
-                    /**
-                     * @brief Set the Mdc Seg Category object
-                     * 
-                     * @throw runtime_error if category is not found
-                     */
-                    void SetMdcSegCategory()
-                    {
-                        mdc_seg_cat = (HCategory*) gHades->getCurrentEvent()->getCategory(catMdcSeg);
-                        if (mdc_seg_cat == nullptr)
-                            throw std::runtime_error("HFiredWires::SetMdcSegCategory() catMdcSeg not available");
-
-                        MdcCatIsNotSet = false;
-                    }
-                    /**
-                     * @brief Set the Inner And Outer Segments object when runing DST
-                     * 
-                     * @param cand 
-                     * @throw runtime_error if the segments are not found
-                     */
-                    void SetInnerAndOuterSegments(HParticleCand *cand)
-                    {
-                        innerSeg = HCategoryManager::getObject(innerSeg,mdc_seg_cat,cand->getInnerSegInd());
-                        outerSeg = HCategoryManager::getObject(outerSeg,mdc_seg_cat,cand->getOuterSegInd());
-
-                        if (innerSeg == nullptr || outerSeg == nullptr)
-                            throw std::runtime_error("HFiredWires::SetInnerAndOuterSegments() inner and/or outer mdc segment is null");
-                    }
-                    /**
-                     * @brief Set the Inner And Outer Segments object when runing HGeant
-                     * 
-                     * @param cand 
-                     * @throw runtime_error if the segments are not found
-                     */
-                    void SetInnerAndOuterSegments(HParticleCandSim *cand)
-                    {
-                        innerSeg = HCategoryManager::getObject(innerSeg,mdc_seg_cat,cand->getInnerSegInd());
-                        outerSeg = HCategoryManager::getObject(outerSeg,mdc_seg_cat,cand->getOuterSegInd());
-
-                        if (innerSeg == nullptr || outerSeg == nullptr)
-                            throw std::runtime_error("HFiredWires::SetInnerAndOuterSegments() inner and/or outer mdc segment is null");
-                    }
-                    /**
-                     * @brief Map wires from HMdcSeg to the std::array<std::vector<int>,24>
-                     * 
-                     */
-                    void SetWires()
-                    {
-                        int ncells = 0;
-
-                        for (const int &layer : WireInfo::halfLayerIndexing)
-                        {
-                            ncells = innerSeg->getNCells(layer);
-                            if (ncells < 1)
-                                continue;
-
-                            for (int cell = 0; cell < ncells; ++cell)
-                                FiredWireCollection.at(layer).push_back(innerSeg->getCell(layer,cell));
-                        }
-                        for (const int &layer : WireInfo::halfLayerIndexing)
-                        {
-                            ncells = outerSeg->getNCells(layer);
-                            if (ncells < 1)
-                                continue;
-
-                            for (int cell = 0; cell < ncells; ++cell)
-                                FiredWireCollection.at(layer+WireInfo::numberOfInnerLayers).push_back(outerSeg->getCell(layer,cell));
-                        }
-                    }
-
                 public:
-                    /**
-                     * @brief Construct a new HFiredWires object. Construct the object once, before the event loop
-                     * 
-                     */
-                    HFiredWires() : MdcCatIsNotSet(true)
-                    {
-                        SetMdcSegCategory();
-                    }
-                    /**
-                     * @brief Construct a new HFiredWires object for this DST track. Invoke this method every new track, before trying to access the wires collection
-                     * 
-                     * @param cand 
-                     */
-                    HFiredWires(HParticleCand *cand)
-                    {
-                        if (MdcCatIsNotSet)
-                            SetMdcSegCategory();
-                        SetInnerAndOuterSegments(cand);
-                        SetWires();
-                    }
-                    /**
-                     * @brief Construct a new HFiredWires object for this HGeant track. Invoke this method every new track, before trying to access the wires collection
-                     * 
-                     * @param cand 
-                     */
-                    HFiredWires(HParticleCandSim *cand)
-                    {
-                        if (MdcCatIsNotSet)
-                            SetMdcSegCategory();
-                        SetInnerAndOuterSegments(cand);
-                        SetWires();
-                    }
                     /**
                      * @brief Get the collection of fired wires for the track specified in the constructor
                      * 
                      * @return std::array<std::vector<int>,WireInfo::numberOfAllLayers>
                      */
-                    std::array<std::vector<int>,WireInfo::numberOfAllLayers> GetWires() const
+                    std::array<std::vector<int>,WireInfo::numberOfAllLayers> GetWires(HMdcSeg *inner, HMdcSeg *outer) const
                     {
-                        return FiredWireCollection;
-                    }
-                    /**
-                     * @brief Prints to the standard output all the indexes of the wires that were hit
-                     * 
-                     */
-                    void PrintWires() const
-                    {
-                        for (const std::size_t &layer : WireInfo::allLayerIndexing)
+                        std::array<std::vector<int>,WireInfo::numberOfAllLayers> FiredWireCollection;
+                        int ncells = 0;
+
+                        for (const int &layer : WireInfo::halfLayerIndexing)
                         {
-                            if (layer == 0)
-                                std::cout << "\n\n---=== Inner Segment ===---\n";
-                            else if (layer == 12)
-                                std::cout << "---=== Outer Segment ===---\n";
+                            ncells = inner->getNCells(layer);
+                            if (ncells < 1)
+                                continue;
 
-                            std::cout << "Layer " << layer+1 << ":\t";
-                            for (const auto &wire : FiredWireCollection.at(layer))
-                                std::cout << wire << "\t";
-
-                            std::cout << "\n";
+                            for (int cell = 0; cell < ncells; ++cell)
+                                FiredWireCollection.at(layer).push_back(inner->getCell(layer,cell));
                         }
-                        std::cout << "\n\n";
+                        for (const int &layer : WireInfo::halfLayerIndexing)
+                        {
+                            ncells = outer->getNCells(layer);
+                            if (ncells < 1)
+                                continue;
+
+                            for (int cell = 0; cell < ncells; ++cell)
+                                FiredWireCollection.at(layer+WireInfo::numberOfInnerLayers).push_back(outer->getCell(layer,cell));
+                        }
+
+                        return FiredWireCollection;
                     }
             };
         }
